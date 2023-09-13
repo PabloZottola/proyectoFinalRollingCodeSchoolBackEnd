9a886e56b6983596547cb250421e3a6717b5ca69
Pablo Zottola <pablozottola66@gmail.com>
2023-09-13 19:03:47 -0300

    Coneccion con la base de datos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { dbConnection } = require("./database/config");
 const app = express();
+
 require("dotenv").config();
+
+dbConnection();
+
 app.use(express.json());
 app.use("/auth", require("./routes/register"));
-
 app.listen(process.env.PORT, () => {
   console.log(`Servidor en el puerto ${process.env.PORT}`);
 });
---
+++ src/database/config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN);
+    console.log("conectado a la base de datos");
+  } catch (error) {
+    console.log("problema con la conexion a la base de datos");
+  }
+};
+
+module.exports = { dbConnection };
