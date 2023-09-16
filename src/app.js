bfdbdbe96fb613024761b19c4261452f10c47c49
Pablo Zottola <pablozottola66@gmail.com>
2023-09-16 11:18:25 -0300

    back conectado con el front

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { dbConnection } = require("./database/config");
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
@@ -7,6 +8,7 @@ require("dotenv").config();
 dbConnection();
 
 app.use(express.json());
+app.use(cors());
 app.use("/auth", require("./routes/register"));
 app.listen(process.env.PORT, () => {
   console.log(`Servidor en el puerto ${process.env.PORT}`);
--- src/controllers/register.controllers.js
+++ src/controllers/register.controllers.js
@@ -2,45 +2,34 @@ const User = require("../model/user-model");
 const bcrypt = require("bcrypt");
 
 async function accountCreation(req, res) {
-  const {
-    firstName,
-    lastName,
-    phone,
-    birthday,
-    email,
-    password,
-    repeatPassword,
-  } = req.body;
+  const { firstName, lastName, phone, email, password, repeatPassword } =
+    req.body;
 
   try {
     const emailExist = await User.findOne({ email });
     const phoneExist = await User.findOne({ phone });
 
     if (
-      checkEmptySpacesRegister_P(
-        firstName,
-        lastName,
-        phone,
-        birthday,
-        email,
-        password,
-        repeatPassword
-      )
+      firstName.trim() === "" ||
+      lastName.trim() === "" ||
+      phone.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === ""
     )
       return res.json({ msg: "Todos los campos son obligatorios." });
-    if (/\d/.test(firstName)) return res.json({ msg: "Nombre invalido." });
-    if (firstName.length < 2) return res.json({ msg: "Nombre invalido." });
-    if (/\d/.test(lastName)) return res.json({ msg: "Apellido invalido." });
-    if (lastName.length < 2) return res.json({ msg: "Apellido invalido." });
-    if (/[a-zA-Z]/.test(phone))
+    if (/\d/.test(firstName) || firstName.length < 2)
+      return res.json({ msg: "Nombre invalido." });
+    if (/\d/.test(lastName) || lastName.length < 2)
+      return res.json({ msg: "Apellido invalido." });
+    if (
+      /[a-zA-Z]/.test(phone) ||
+      phone.length < 9 ||
+      phone.length > 10 ||
+      phoneExist
+    )
       return res.json({ msg: "Numero de telefono invalido." });
-    if (phone.length < 9 || phone.length > 10)
-      return res.json({ msg: "Numero de telefono invalido." });
-    if (phoneExist) return res.json({ msg: "Numero de telefono invalido." });
-    if (/[a-zA-Z]/.test(birthday))
-      return res.json({ msg: "Numero de cumpleaños invalido." });
-    if (!validateEmail(email)) return res.json({ msg: "E-mail no valido." });
-    if (emailExist) return res.json({ msg: "E-mail no valido." });
+    if (!validateEmail(email) || emailExist)
+      return res.json({ msg: "E-mail no valido." });
     if (password.length <= 5) return res.json({ msg: "Contraseña invalida." });
     if (password !== repeatPassword)
       return res.json({ msg: "Las contraseña no coinciden" });
@@ -55,30 +44,6 @@ async function accountCreation(req, res) {
     });
   } catch (error) {
     console.log(error);
-  }
-}
-
-function checkEmptySpacesRegister_P(
-  firstName,
-  lastName,
-  phone,
-  birthday,
-  email,
-  password,
-  repeatPassword
-) {
-  if (
-    firstName.trim() === "" ||
-    lastName.trim() === "" ||
-    phone.trim() === "" ||
-    birthday.trim() === "" ||
-    email.trim() === "" ||
-    password.trim() === "" ||
-    repeatPassword.trim() === ""
-  ) {
-    return true;
-  } else {
-    return false;
   }
 }
 function validateEmail(email) {
