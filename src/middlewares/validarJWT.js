89788277ab6233fe08927cd8674fcab233f6520e
Pablo Zottola <pablozottola66@gmail.com>
2023-09-27 16:36:28 -0300

    Corregiendo errores

--- src/middlewares/validarJWT.js
+++ src/middlewares/validarJWT.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const validarJWT = (req, res, next) => {
+function validarJWT(req, res, next) {
   const token = req.header("x-token");
 
   if (!token) {
@@ -11,15 +11,14 @@ const validarJWT = (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.SECRET_JWT);
-    console.log(payload);
   } catch (error) {
     return res.status(401).json({
-      msg: "token no valido",
+      msg: "Token no valido.",
     });
   }
 
   next();
-};
+}
 
 module.exports = {
   validarJWT,
